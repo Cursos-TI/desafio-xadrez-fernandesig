cab61af4844c84f895f7ca776f986e090f921b41
Igor Fernandes <63010702+fernandesig@users.noreply.github.com>
2025-07-10 00:33:43 +0000

    - Nivel avançado - Movimentação do cavalo feita com loop aninhado (For e Do-While)

--- xadrez.c
+++ xadrez.c
@@ -2,28 +2,60 @@
 
 int main() {
 
-    int i, moveBispo = 1, moveRainha = 1;
+    int i, moveCavalo, moveBispo = 1, moveRainha = 1;
 
 
     // Movimento Torre em for
+    printf("Movimentacao Torre\n");
+    printf("\n");
     for (i = 1; i <= 5; i++) {
         printf("Direita\n");
     }
+    printf("\n");
 
-    // Movimento Bispo Do-While
+    // Movimento Bispo
+    printf("Movimentacao Bispo\n");
+    printf("\n");
+
     do
     {        
         printf("Cima, direita\n");
+        
         moveBispo++;        
 
     } while (moveBispo <= 5);
+    printf("\n");
 
-    // Movimento Rainha While
+    // Movimento rainha
+    printf("Movimentacao Rainha\n");
+    printf("\n");
+
     while (moveRainha <= 8) {        
         printf("Esquerda\n");
+        
         moveRainha++;  
     }
-       
+    printf("\n");   
+
+
+    // Movimento Cavalo   
+
+    printf("Movimentacao Cavalo\n");
+    printf("\n");    
+
+    for (int j = 0; j < 1; j++)
+    {
+        int k = 0;
+
+        do
+        {
+            printf("Baixo\n");
+            k++;            
+        } while (k < 2 );
+
+        printf ("Esquerda");        
+               
+    }
 
     return 0;
 }
